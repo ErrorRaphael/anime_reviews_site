52e15a614478ac724b6f04a35ec3a7c5375eab5e
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-04 19:51:14 -0300

    Refactor NavMenu

---
+++ Shared/NavMenu.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Site_anime.Shared
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+        private string? NavMenuCssClass => collapseNavMenu ? "navbar-expand" : string.Empty;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
