eb902c002d652002eaa2d514da1e851d83973a7a
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-08 15:43:59 -0300

    Create Model and ReviewDto

---
+++ Site anime.Model/Dto/ReviewDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site_anime.Model.Dto
+{
+    public class ReviewDto
+    {
+        public string Anime { get; set; }
+        public string Image { get; set; }
+        public string Link { get; set; }
+        public string Content { get; set; }
+    }
+}
