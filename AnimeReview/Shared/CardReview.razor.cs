54453af6f3bce332dbe34436ef9fc8af9539e555
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-14 08:34:53 -0300

    add service to CardReview

--- AnimeReview/Shared/CardReview.razor.cs
+++ AnimeReview/Shared/CardReview.razor.cs
@@ -1,9 +1,19 @@
 ﻿using Site_anime.Model.Dto;
+using Site_anime.Service;
 
 namespace Site_anime.Shared
 {
 	public partial class CardReview
 	{
 		public List<ReviewDto> Reviews { get; set; }
+
+		private ReviewService _service;
+
+		public CardReview()
+		{
+			_service = new();
+
+			Reviews = _service.FindAllReviews();
+		}
 	}
 }
