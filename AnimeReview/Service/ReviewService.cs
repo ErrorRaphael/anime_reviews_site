4e15db43c57687de53e07e8d2d8775a05acac3d7
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-14 08:34:40 -0300

    Create ReviewService

---
+++ AnimeReview/Service/ReviewService.cs
@@ -0,0 +1,30 @@
+﻿using Site_anime.Model.Dto;
+
+namespace Site_anime.Service
+{
+    public class ReviewService
+    {
+        public ReviewService()
+        {
+
+        }
+
+        public List<ReviewDto> FindAllReviews()
+        {            
+            return SimulateList();
+        }
+        private List<ReviewDto> SimulateList()
+        {
+            List<ReviewDto> list = new List<ReviewDto>();
+
+            list.Add(new()
+            {
+                Anime = "Sword Art Online",
+                Content = "The story of the first season follows the adventures of Kazuto \"Kirito\" Kirigaya and Asuna Yuuki, two players who are trapped in the virtual world of \"Sword Art Online\" (SAO). They are tasked to clear all 100 Floors and defeat the final boss in order to be freed from the game.",
+                Image = "https://img.comunidades.net/dig/digitalnews/5fe886f3daa6485efecf1ea6dc5207931343265548_full.jpg"
+            });
+
+            return list;
+        };
+    }
+}
