927c2eeb1f955de5a77306c8c2ab4b3df46bb4a9
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-14 16:56:59 -0300

    Add reviewService

--- AnimeReview/Program.cs
+++ AnimeReview/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Site_anime;
+using Site_anime.Service;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddTransient<ReviewService>();
 
 await builder.Build().RunAsync();
